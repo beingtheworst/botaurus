f97d9ef91900ebc7d66e539bed558cd2123de64c
Rinat Abdullin <rinat.abdullin@gmail.com>
2016-02-11 21:25:39 +0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sv3
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject sv3 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [metosin/compojure-api "1.0.0-RC1"  ]
+                 [prismatic/schema "1.0.4" ]
+                 [metosin/ring-swagger-ui "2.1.4-0"]
+                 [metosin/ring-swagger "0.22.3"]
+                 ]
+  :ring {:handler sv3.core/app}
+  :uberjar-name "server.jar"
+  
+
+  :profiles {:dev {
+                   :dependencies [[javax.servlet/servlet-api "2.5"]]
+                   :plugins [[lein-ring "0.9.6"]]
+                   }}
+
+
+
+  )
